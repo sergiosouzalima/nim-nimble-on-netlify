18b4e7bdfa2c895c9ec7196014ed3417a36e14b5
Sergio Lima <sergiosouzalima@gmail.com>
2022-06-19 08:03:42 -0300

    Update repo

--- build.sh
+++ build.sh
@@ -6,7 +6,9 @@ set -u
 src_dir="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 source "$src_dir/scripts/installnim.sh"
 
-install_nim_check "1.6.6"
+#install_nim_check "1.6.6"
+curl https://nim-lang.org/choosenim/init.sh -sSf | sh
 nim -v
+choosenim --version
 nimble install xml -n
 nim c -r -d:ssl --verbosity:0 --hints:off -d:danger -d:lto --opt:speed mainapp

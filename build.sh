936c5ca4962e34c80ee101a0e5f89d8731dd080d
Sergio Lima <sergiosouzalima@gmail.com>
2022-06-19 12:37:24 -0300

    Update repo

--- build.sh
+++ build.sh
@@ -5,6 +5,12 @@ set -u
 
 src_dir="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 
+sudo apt update -y
+
+sudo apt upgrade -y
+
+sudo apt-get install build-essential -y
+
 # source "$src_dir/scripts/installnim.sh"
 
 #install_nim_check "1.6.6"

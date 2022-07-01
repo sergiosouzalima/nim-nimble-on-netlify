fb4d0907425468dbf111ff001e22ed7dd9883068
Sergio Lima <sergiosouzalima@gmail.com>
2022-07-01 17:28:00 -0300

    Added unit tests

--- tests/test1.nim
+++ tests/test1.nim
@@ -15,20 +15,23 @@ import ../src/mainapp
 suite "func updatedAtFormat":
 
   let currentTimeStamp1: string = "Sun, 26 Jun 2022 10:54:12 Z"
-  let test1 = fmt"""when date is {currentTimeStamp1} it returns '26/Jun/2022 10:54:12' """
+  let test1 = fmt"""{currentTimeStamp1} it returns '26/Jun/2022 10:54:12' """
 
   let currentTimeStamp2: string = "Sat, 18 Jun 2022 05:46:51 Z"
-  let test2 = fmt"""when date is {currentTimeStamp2} it returns '18/Jun/2022 05:46:51' """
+  let test2 = fmt"""{currentTimeStamp2} it returns '18/Jun/2022 05:46:51' """
 
   let currentTimeStamp3: string = "Fri, 17 Jun 2022 04:16:12 Z"
-  let test3 = fmt"""when date is {currentTimeStamp3} it returns '17/Jun/2022 04:16:12' """
+  let test3 = fmt"""{currentTimeStamp3} it returns '17/Jun/2022 04:16:12' """
 
-  test test1:
-    check updatedAtFormat(currentTimeStamp1) == "26/Jun/2022 10:54:12"
+  let currentTimeStamp4: string = "Fri, 01 Jul 2022 05:25:00 Z"
+  let test4 = fmt"""{currentTimeStamp3} it returns '01/Jul/2022 05:25:00' """
 
-  test test2:
-    check updatedAtFormat(currentTimeStamp2) == "18/Jun/2022 05:46:51"
-
-  test test3:
-    check updatedAtFormat(currentTimeStamp3) == "17/Jun/2022 04:16:12"
-
+  suite "when date is:":
+    test test1:
+      check updatedAtFormat(currentTimeStamp1) == "26/Jun/2022 10:54:12"
+    test test2:
+      check updatedAtFormat(currentTimeStamp2) == "18/Jun/2022 05:46:51"
+    test test3:
+      check updatedAtFormat(currentTimeStamp3) == "17/Jun/2022 04:16:12"
+    test test4:
+      check updatedAtFormat(currentTimeStamp4) == "01/Jul/2022 05:25:00"
